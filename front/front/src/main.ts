6492252b5da9db933b7640ec60a7e9f7eadd7ba8
TK <yusuke.taki@hotmail.com>
2021-06-06 15:34:30 +0900

    add vue router

--- front/front/src/main.ts
+++ front/front/src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as  vr from 'vue-router'
+''
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+const routes:vr.RouteRecordRaw[] = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+  ]
+  
 
-createApp(App).mount('#app')
+const router = vr.createRouter(
+    {
+     history:vr.createWebHistory(),
+     routes:routes
+    }
+)
+createApp(App).use(router).mount('#app')

b21e68d2d5bcb6dcbfafb8a245a191e3c65a09c4
TK <yusuke.taki@hotmail.com>
2021-06-06 15:48:14 +0900

    run prettier

--- front/front/src/main.ts
+++ front/front/src/main.ts
@@ -1,19 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import * as  vr from 'vue-router'
-''
+import * as vr from 'vue-router'
+
 const Home = { template: '<div>Home</div>' }
 const About = { template: '<div>About</div>' }
-const routes:vr.RouteRecordRaw[] = [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
-  ]
-  
+const routes: vr.RouteRecordRaw[] = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
 
-const router = vr.createRouter(
-    {
-     history:vr.createWebHistory(),
-     routes:routes
-    }
-)
+const router = vr.createRouter({
+  history: vr.createWebHistory(),
+  routes: routes,
+})
 createApp(App).use(router).mount('#app')

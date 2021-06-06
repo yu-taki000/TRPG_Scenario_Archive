b6a54d06971c09cb25149390a68cc3b17eac3e53
TK <yusuke.taki@hotmail.com>
2021-06-06 21:50:29 +0900

    change linter rules

--- front/front/src/main.ts
+++ front/front/src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import * as vr from 'vue-router'
+import { createApp } from 'vue';
+import * as vr from 'vue-router';
+import App from './App.vue';
 
-const Home = { template: '<div>Home</div>' }
-const About = { template: '<div>About</div>' }
+const Home = { template: '<div>Home</div>' };
+const About = { template: '<div>About</div>' };
 const routes: vr.RouteRecordRaw[] = [
   { path: '/', component: Home },
   { path: '/about', component: About },
-]
+];
 
 const router = vr.createRouter({
   history: vr.createWebHistory(),
-  routes: routes,
-})
-createApp(App).use(router).mount('#app')
+  routes,
+});
+createApp(App).use(router).mount('#app');

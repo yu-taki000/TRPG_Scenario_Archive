c67fc8b6adb181520f2659a3e7c4451f1fc61531
TK <yusuke.taki@hotmail.com>
2021-06-09 08:04:51 +0900

    add save

--- front/front/src/components/pages/upload.vue
+++ front/front/src/components/pages/upload.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-fluid sa-ly-width">
-    <div class="p-field">
+    <div class="p-field p-m-4">
       <span class="p-float-label">
         <InputText
           id="inputtext"
@@ -10,43 +10,60 @@
         <label for="inputtext">シナリオ名</label>
       </span>
     </div>
-    <div class="p-field">
+    <div class="p-field p-m-4">
       <span class="p-float-label">
         <Textarea
           id="textarea"
           v-model="value2"
-          :autoResize="true"
+          :auto-resize="true"
+          class="sa-co-width"
         />
         <label for="textarea">シナリオの説明</label>
       </span>
     </div>
-  </div>
-  <div>
-    <div class="p-grid">
-      <div class="p-col">
-        <FileUpload
-          name="preInfo"
-          chooseLabel="公開情報"
-          url="./upload"
-          mode="basic"
-          accept="image/*"
-          :maxFileSize="10000000"
-          :fileLimit="1"
-        />
-      </div>
-      <div class="p-col">
-        <FileUpload
-          name="senarioData"
-          chooseLabel="シナリオ(zip)"
-          url="./upload"
-          mode="basic"
-          accept=".zip"
-          :maxFileSize="10000000"
-          :fileLimit="1"
-        />
-      </div>
-      <div class="p-col">
-        <Button label="投稿" />
+    <div class="p-field p-m-4 p-text-left">
+      <FileUpload
+        name="demo[]"
+        choose-label="公開情報"
+        url="./upload.php"
+        :multiple="true"
+        accept="image/*"
+        :max-file-size="10000000"
+        :show-upload-button="false"
+        :show-cancel-button="false"
+        :file-limit="1"
+        preview-width="200"
+        @upload="onUpload"
+      >
+        <template #empty>
+          <p>Drag and drop files to here to upload.</p>
+        </template>
+      </FileUpload>
+    </div>
+    <div class="p-field p-m-4 p-text-left">
+      <FileUpload
+        name="senarioData"
+        choose-label="シナリオ(zip)"
+        url="./upload.php"
+        :multiple="true"
+        accept=".zip"
+        :max-file-size="10000000"
+        :show-upload-button="false"
+        :show-cancel-button="false"
+        :file-limit="1"
+        preview-width="200"
+        @upload="onUpload"
+      >
+        <template #empty>
+          <p>Drag and drop files to here to upload.</p>
+        </template>
+      </FileUpload>
+    </div>
+    <div class="p-grid p-jc-end">
+      <div class="p-col-2">
+        <div class="box">
+          <Button label="投稿" />
+        </div>
       </div>
     </div>
   </div>
@@ -77,6 +94,9 @@ export default defineComponent({
     width: 80%;
     margin-left:auto;
     margin-right:auto;
+  }
+  .sa-co-width{
+    width:100%
   }
 
 </style>

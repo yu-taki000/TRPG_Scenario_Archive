6644a60658bb0a413217afb6e3c5cfb527960200
TK <yusuke.taki@hotmail.com>
2021-06-16 01:16:21 +0900

    add login button in login

--- front/front/src/components/pages/login.vue
+++ front/front/src/components/pages/login.vue
@@ -1,18 +1,37 @@
 <template>
-  <h1>login</h1>
-  testLoginMessage
+  <div>
+    <h1>login</h1>
+    <button @click="handleLogin">
+      Log in
+    </button>
+    AuthInfo:{{ auth }}
+    <button @click="handleLogout">
+      Log out
+    </button>
+    Info:{{ auth.user.value }}
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue';
+import { inject, defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Login',
   props: {
   },
-  setup: () => {
-    const count = ref(0);
-    return { count };
+  setup() {
+    const auth = inject<any>('$auth');
+
+    return {
+      auth,
+      handleLogin: () => {
+        auth.loginWithRedirect();
+      },
+      handleLogout: () => {
+        auth.logout({
+          returnTo: window.location.origin,
+        });
+      },
+    };
   },
-});
-</script>
+}); </script>
--- front/front/src/main.ts
+++ front/front/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import * as router from '@/router/index';
+// Auth Block
+import * as Auth0Plugin from '@/auth'
+import authConfig from '@/auth/auth_config.json'
+
+// PrimeVue Block
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import textArea from 'primevue/textarea';
@@ -9,15 +14,24 @@ import Button from 'primevue/button';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 
+
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
 import 'primeflex/primeflex.css';
 
-createApp(App)
+
+const app = createApp(App)
   .use(router.router)
-  .use(PrimeVue)
-  .component('InputText', InputText)
+  .use(PrimeVue);
+
+Auth0Plugin.setupAuth(authConfig, (appState: any) => {
+  router.router.push(appState && appState.targetUrl ? appState.targetUrl : '/')
+}).then((auth) => {
+  app.use(auth)
+})
+
+app.component('InputText', InputText)
   .component('textArea', textArea)
   .component('FileUpload', FileUpload)
   .component('Button', Button)
